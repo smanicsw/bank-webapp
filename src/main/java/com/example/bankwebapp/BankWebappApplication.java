2e7577c8778a95a26328286edd03750c4365035c
Sergiu Manic <sergiu.manic15@gmail.com>
2023-10-02 16:42:24 +0100

    first commit

---
+++ src/main/java/com/example/bankwebapp/BankWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankwebapp/BankWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
